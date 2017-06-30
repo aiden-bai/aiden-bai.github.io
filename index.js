ae1bad9c654efedf6c9417b53412f96c388985bc
Aiden Bai <aiden.bai05@protonmail.com>
2017-06-30 14:48:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,93 @@
+var current = "X";
+
+        window.onload = function() {
+
+            /* when inner_1 is clicked, change the value to X or O */
+            document.getElementById("inner_1").onclick = function() {
+                document.getElementById("inner_1").innerHTML = current;
+                /* ..and then update the X to an O or vice versa */
+                if (current == "X") {
+                    current = "O";
+                } else if (current == "O") {
+                    current = "X";
+                }
+            }
+            
+            document.getElementById("inner_2").onclick = function() {
+                document.getElementById("inner_2").innerHTML = current;
+                /* ..and then update the X to an O or vice versa */
+                if (current == "X") {
+                    current = "O";
+                } else if (current == "O") {
+                    current = "X";
+                }
+            }
+            
+            document.getElementById("inner_3").onclick = function() {
+                document.getElementById("inner_3").innerHTML = current;
+                /* ..and then update the X to an O or vice versa */
+                if (current == "X") {
+                    current = "O";
+                } else if (current == "O") {
+                    current = "X";
+                }
+            }
+            
+            document.getElementById("inner_4").onclick = function() {
+                document.getElementById("inner_4").innerHTML = current;
+                /* ..and then update the X to an O or vice versa */
+                if (current == "X") {
+                    current = "O";
+                } else if (current == "O") {
+                    current = "X";
+                }
+            }
+            document.getElementById("inner_5").onclick = function() {
+                document.getElementById("inner_5").innerHTML = current;
+                /* ..and then update the X to an O or vice versa */
+                if (current == "X") {
+                    current = "O";
+                } else if (current == "O") {
+                    current = "X";
+                }
+            }
+            document.getElementById("inner_6").onclick = function() {
+                document.getElementById("inner_6").innerHTML = current;
+                /* ..and then update the X to an O or vice versa */
+                if (current == "X") {
+                    current = "O";
+                } else if (current == "O") {
+                    current = "X";
+                }
+            }
+            document.getElementById("inner_7").onclick = function() {
+                document.getElementById("inner_7").innerHTML = current;
+                /* ..and then update the X to an O or vice versa */
+                if (current == "X") {
+                    current = "O";
+                } else if (current == "O") {
+                    current = "X";
+                }
+            }
+            document.getElementById("inner_8").onclick = function() {
+                document.getElementById("inner_8").innerHTML = current;
+                /* ..and then update the X to an O or vice versa */
+                if (current == "X") {
+                    current = "O";
+                } else if (current == "O") {
+                    current = "X";
+                }
+            }
+            document.getElementById("inner_9").onclick = function() {
+                document.getElementById("inner_9").innerHTML = current;
+                /* ..and then update the X to an O or vice versa */
+                if (current == "X") {
+                    current = "O";
+                } else if (current == "O") {
+                    current = "X";
+                }
+            }
+            /* wire up boxes 2 & 3 for X and O */
+
+        }
 
